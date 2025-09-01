48e4d485c1d95600aa9afb1419d7e39f4130d743
Vagner Jorge Souza Feitosa <svagnerjorge@gmail.com>
2025-09-01 01:37:36 -0300

    server

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 import { getPlant , getPlants, createPlant } from './db.js';
 
-app.use(express.json());
 
 app.get("/plantas", async(req, res) => {
     const plantas = await getPlants();
